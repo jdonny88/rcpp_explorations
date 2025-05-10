87abe9937c9c04b11e550616d2f5c74b3b923aac
Jay Doe <jay@example.com>
2025-05-10 16:08:26 +1000

    Added C++ primitive

--- src/module.cpp
+++ src/module.cpp
@@ -4,10 +4,12 @@ RCPP_EXPOSED_CLASS(Demo)
 
 class Demo {
     public:
+        int myInt;
         Demo() {}
 };
 
 RCPP_MODULE(demo_module) {
     Rcpp::class_<Demo>("Demo")
         .constructor();
+        .field("myInt", &Demo::myInt);
 }
