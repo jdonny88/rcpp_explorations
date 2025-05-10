39907e6a94dbe431b0ffa339e986b2d3baaad111
Jay Doe <jay@example.com>
2025-05-10 22:22:06 +1000

    back to basic types

--- src/module.cpp
+++ src/module.cpp
@@ -1,5 +1,4 @@
 #include <Rcpp.h>
-#include <string>
 
 RCPP_EXPOSED_CLASS(Demo)
 
@@ -7,14 +6,12 @@ class Demo {
     public:
         int myInt;
         double myDouble;
-        std::string myString;
-        Demo() : myInt(0), myDouble(0.0), myString("hello") {}
+        Demo() : myInt(0), myDouble(0.0) {}
 };
 
 RCPP_MODULE(demo_module) {
     Rcpp::class_<Demo>("Demo")
         .constructor()
         .field("myInt", &Demo::myInt)
-        .field("myDouble", &Demo::myDouble)
-        .field("myString", &Demo::myString);
+        .field("myDouble", &Demo::myDouble);
 }
