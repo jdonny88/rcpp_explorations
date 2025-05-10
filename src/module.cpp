9b7032059798db07f16e557eff24616ca93975dc
Jay Doe <jay@example.com>
2025-05-10 16:21:54 +1000

    primitive type with getter and setter

--- src/module.cpp
+++ src/module.cpp
@@ -6,10 +6,15 @@ class Demo {
     public:
         int myInt;
         Demo() : myInt(0) {}
+
+        int getInt() const { return myInt; }
+        void setInt(int value) { myInt = value; }
 };
 
 RCPP_MODULE(demo_module) {
     Rcpp::class_<Demo>("Demo")
         .constructor();
-        .field("myInt", &Demo::myInt);
+        .field("myInt", &Demo::myInt)
+        .method("getInt", &Demo::getInt)
+        .method("setInt", &Demo::setInt);
 }
