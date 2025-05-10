d1530abe81005634fc7f4f4555dfe436ea1abe90
Jay Doe <jay@example.com>
2025-05-10 20:45:55 +1000

    fields only test

--- src/module.cpp
+++ src/module.cpp
@@ -1,20 +1,23 @@
 #include <Rcpp.h>
+#include <string>
+#include <vector>
 
 RCPP_EXPOSED_CLASS(Demo)
 
 class Demo {
     public:
         int myInt;
-        Demo() : myInt(0) {}
-
-        int getInt() const { return myInt; }
-        void setInt(int value) { myInt = value; }
+        double myDouble;
+        std::string myString;
+        std::vector<double> myVec;
+        Demo() : myInt(0), myDouble(2.0), myString("hello"), myVec({1.1, 2.2, 3.3}) {}
 };
 
 RCPP_MODULE(demo_module) {
     Rcpp::class_<Demo>("Demo")
         .constructor()
         .field("myInt", &Demo::myInt)
-        .method("getInt", &Demo::getInt)
-        .method("setInt", &Demo::setInt);
+        .field("myDouble", &Demo::myDouble)
+        .field("myString", &Demo::myString)
+        .field("myVec", &Demo::myVec);
 }
